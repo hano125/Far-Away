6cd313ecf3a4e06c2d46cf0753030e1821d98709
mohannad abbas <mohannad.abbas.dev@gmail.com>
2025-06-29 09:55:05 +0300

    make ui with rediring list from const data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "FPSC_Soket", quantity: 12, packed: false },
+  { id: 4, description: "Tree", quantity: 12, packed: true },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+function Logo() {
+  return <h1> 👜 Far Away 🌴</h1>;
+}
+function Form() {
+  return (
+    <form className="add-form">
+      <h3>Where whuld you like to trip 🌴</h3>
+      <input type="text" placeholder="Destination" />
+      <button>Add</button>
+    </form>
+  );
+}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+    </li>
+  );
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>we far away with out come back 🤚</em>
+    </footer>
+  );
+}
